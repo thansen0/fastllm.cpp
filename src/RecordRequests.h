33db746b28d605487d47b84fba4cee766117fd48
_ <thomash1996@live.com>
2024-11-25 01:47:42 -0800

    moved towards base class implementation

--- src/RecordRequests.h
+++ src/RecordRequests.h
@@ -3,17 +3,33 @@
 
 #include <fstream>
 
-class RecordRequests {
+class RecordRequestsBase {
+public:
+    virtual ~RecordRequestsBase() = default;
+
+    virtual void recordLLMRequest(const std::string& prompt, const std::string& result, const std::string& user_uuid) = 0;
+};
+
+
+
+class RecordRequests : public RecordRequestsBase {
 private:
     std::ofstream logFile;
 
 public:
     RecordRequests(const std::string& filePath);
+    virtual ~RecordRequests() override;
 
-    ~RecordRequests();
-
-    void recordLLMRequest(const std::string& prompt, const std::string& result, const std::string& user_uuid);
+    virtual void recordLLMRequest(const std::string& prompt, const std::string& result, const std::string& user_uuid) override;
 };
 
+
+class RecordRequestsNoRecord : public RecordRequestsBase {
+public:
+    virtual void recordLLMRequest(const std::string& prompt, const std::string& result, const std::string& user_uuid) {
+        // Doesn't record information
+        return;
+    }
+};
 
 #endif // RECORDREQUESTS_H
