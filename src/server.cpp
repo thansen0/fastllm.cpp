4151dba4d45fc9d9f799d84341cf8b8aace7cb6b
thomas <thomash1996@live.com>
2025-06-13 22:40:56 -0600

    fixed deprecated warnings

--- src/server.cpp
+++ src/server.cpp
@@ -66,14 +66,13 @@ public:
         model_params = llama_model_default_params();
         model_params.n_gpu_layers = n_gpu_layers; // number of layers to offload to the GPU
 
-        model = llama_load_model_from_file(model_path.c_str(), model_params);
+        model = llama_model_load_from_file(model_path.c_str(), model_params);
 
         if (model == NULL) {
             fprintf(stderr , "%s: error: unable to load model\n" , __func__);
         }
 
         vocab = llama_model_get_vocab(model);
-        // vocab = const_cast<llama_vocab*>(llama_model_get_vocab(model));
 
         if (!DEBUG_MODE) {
             // needed because llama_context_params can't disable logging
@@ -82,7 +81,7 @@ public:
     }
 
     ~AskLLMQuestionServiceImpl() {
-        llama_free_model(model);
+        llama_model_free(model);
 
         // save any new keys to the toml file
         writeConfigKeyEnforcer("../config/config.toml", ke);
@@ -140,7 +139,7 @@ public:
         ctx_params.no_perf = false;
 
 
-        llama_context * ctx = llama_new_context_with_model(model, ctx_params);
+        llama_context * ctx = llama_init_from_model(model, ctx_params);
 
         if (ctx == NULL) {
             if (DEBUG_MODE)
@@ -192,7 +191,7 @@ public:
                 new_token_id = llama_sampler_sample(smpl, ctx, -1);
 
                 // is it an end of generation?
-                if (llama_token_is_eog(vocab, new_token_id)) {
+                if (llama_vocab_is_eog(vocab, new_token_id)) {
                     break;
                 }
 
@@ -324,7 +323,7 @@ public:
         ctx_params.no_perf = false;
 
 
-        llama_context * ctx = llama_new_context_with_model(model, ctx_params);
+        llama_context * ctx = llama_init_from_model(model, ctx_params);
 
         if (ctx == NULL) {
             if (DEBUG_MODE)
@@ -380,7 +379,7 @@ public:
                 new_token_id = llama_sampler_sample(smpl, ctx, -1);
 
                 // is it an end of generation?
-                if (llama_token_is_eog(vocab, new_token_id)) {
+                if (llama_vocab_is_eog(vocab, new_token_id)) {
                     break;
                 }
 
